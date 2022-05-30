cfc2aa033489d1fdae110f327b23bf05fa2da78e
AlvaroJ4 <alvaro.pereiraJ4@gmail.com>
2022-05-30 15:13:20 -0300

    Máscara monetária

--- Validacao-JavaScript/js/app.js
+++ Validacao-JavaScript/js/app.js
@@ -4,19 +4,15 @@ const inputs = document.querySelectorAll('input')
 
 inputs.forEach(input => {
     if(input.dataset.tipo === 'preco'){
-        SimpleMaskMoney.setMask(input,{
-            afterFormat(e) { console.log('afterFormat', e); },
-            allowNegative: false,
-            beforeFormat(e) { console.log('beforeFormat', e); },
-            negativeSignAfter: false,
-            prefix: '',
-            suffix: '',
+        SimpleMaskMoney.setMask(input, {
+            prefix: 'R$ ',
             fixed: true,
             fractionDigits: 2,
             decimalSeparator: ',',
             thousandsSeparator: '.',
-            cursor: 'move'
+            cursor: 'end'
         } )
+
     }
 
     input.addEventListener('blur', (evento) => {
