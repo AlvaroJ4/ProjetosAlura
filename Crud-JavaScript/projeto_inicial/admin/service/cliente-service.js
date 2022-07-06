04768e051eb6443d552fa6278bf100a62544d24c
AlvaroJ4 <alvaro.pereiraJ4@gmail.com>
2022-07-06 16:24:49 -0300

    Api

--- Crud-JavaScript/projeto_inicial/admin/service/cliente-service.js
+++ Crud-JavaScript/projeto_inicial/admin/service/cliente-service.js
@@ -1,3 +1,22 @@
+const criarNovaLinha = () => {
+    const linhaNovaCliente = document.createElement('tr')
+    const conteudo =  
+`
+    <td class="td" data-td>${nome}</td>
+    <td>${email}</td>
+    <td>
+        <ul class="tabela__botoes-controle">
+            <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+            <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+        </ul>
+    </td> 
+`
+    linhaNovaCliente.innerHTML = conteudo
+    return linhaNovaCliente
+}
+
+const tabela = document.querySelector('[data-tabela]')
+
 const http = new XMLHttpRequest();
 http.open('GET', 'http://localhost:3000/profile')
 
